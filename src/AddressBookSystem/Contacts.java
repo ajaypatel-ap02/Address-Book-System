a2c86f0c81f09e47f39946cac5128042f3e3d764
ajaypatel-ap02 <26patilajay@gmail.com>
2024-05-07 00:49:36 +0530

    [ADDED] : Ajay - Address book project is started

---
+++ src/AddressBookSystem/Contacts.java
@@ -0,0 +1,13 @@
+package AddressBookSystem;
+
+public class Contacts {
+    private String fName;
+    private String lName;
+    private String city;
+    private String address;
+    private String zip;
+    private String state;
+    private String eMail;
+    private String phoneNumber;
+
+}
